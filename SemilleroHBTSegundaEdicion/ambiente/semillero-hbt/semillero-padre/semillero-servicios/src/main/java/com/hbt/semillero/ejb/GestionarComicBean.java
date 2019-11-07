5403513a86fd282805271e71d685d1e309def32b
gustavoArias1 <guandres-94@hotmail.com>
2019-11-06 22:05:00 -0500

    actualizaciones clase gestionarcomicbean

---
+++ SemilleroHBTSegundaEdicion/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/ejb/GestionarComicBean.java
@@ -0,0 +1,154 @@
+/**
+ * GestionarComicBean.java
+ */
+package com.hbt.semillero.ejb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.hbt.semillero.dto.ComicDTO;
+import com.hbt.semillero.entidades.Comic;
+
+/**
+ * <b>Descripción:<b> Clase que determina
+ * <b>Caso de Uso:<b> 
+ * @author mr-robot
+ * @version 
+ */
+@Stateless
+public class GestionarComicBean  implements IGestionarComicLocal{
+	@PersistenceContext
+    private EntityManager em;
+	
+	//TODO
+	/**
+	 * 
+	 *agregar interfaz
+	 */
+	
+	/**
+	 * 
+	 * @see com.hbt.semillero.ejb.IGestionarComicLocal#crearComic(com.hbt.semillero.dto.ComicDTO)
+	 */
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+	public void crearComic(ComicDTO comicDTO) {
+		 Comic comic = convertirComicDTOToComic(comicDTO);
+	     em.persist(comic);
+	}
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void modificarComic(ComicDTO comicModificar) {
+		Comic comic = new Comic();
+	    //comic.setId(comicModificar.getId());
+	     
+	     /**
+	      * TODO 
+	      * llenar el resto de los datos
+	      */
+		em.merge(comicModificar);
+	}
+	
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public ComicDTO consultarComic(String id) {
+		Comic comic = em.find(Comic.class, id);
+		ComicDTO comicDTO = convertirComicToComicDTO(comic);
+		return comicDTO;
+		
+	}
+	
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public List<ComicDTO> consultarComic() {
+		em.createNativeQuery("SELECT C FROM COMIC").getResultList();
+		List<Comic> resultados = (List<Comic>) em.createQuery("select c from Comic").getResultList();
+		return null;
+		
+	}
+
+	/** 
+	 * @see com.hbt.semillero.ejb.IGestionarComicLocal#modificarComic(java.lang.Long, java.lang.String, com.hbt.semillero.dto.ComicDTO)
+	 */
+	@Override
+	//@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	//  TODO cual es el resultado de llamar este metodo si la transaccion
+	public void modificarComic(Long id, String nombre, ComicDTO comicNuevo) {
+		Comic comicModificar;
+		if (comicNuevo==null) {
+			comicModificar = em.find(Comic.class, id);
+		}else {
+			comicModificar = convertirComicDTOToComic(comicNuevo);
+		}
+		//TODO validar si comicmodificar llego con datos 
+		comicModificar.setNombre(nombre);
+		em.merge(comicModificar);
+	}
+
+	/** 
+	 * @see com.hbt.semillero.ejb.IGestionarComicLocal#eliminarComic(java.lang.Long)
+	 */
+	@Override
+	public void eliminarComic(Long idComic) {
+		
+		
+	}
+
+	/** 
+	 * @see com.hbt.semillero.ejb.IGestionarComicLocal#consultarComics()
+	 */
+	@Override
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+	public List<ComicDTO> consultarComics() {
+		List<ComicDTO> resultadosComicDTO = new ArrayList<ComicDTO>();
+		List<Comic> resultados = em.createQuery("select c from Comic c").getResultList();
+		
+		for (Comic comic : resultados) {
+			resultadosComicDTO.add(convertirComicToComicDTO(comic));
+		}
+		return resultadosComicDTO;
+	}
+	
+	private Comic convertirComicDTOToComic(ComicDTO comicDTO) {
+		Comic comic = new Comic();
+        if(comicDTO.getId()!=null) {
+            comic.setId(Long.parseLong(comicDTO.getId()));
+        }
+        comic.setNombre(comicDTO.getNombre());
+        comic.setEditorial(comicDTO.getEditorial());
+        comic.setTematicaEnum(comicDTO.getTematicaEnum());
+        comic.setColeccion(comicDTO.getColeccion());
+        comic.setNumeroPaginas(comicDTO.getNumeroPaginas());
+        comic.setPrecio(comicDTO.getPrecio());
+        comic.setAutores(comicDTO.getAutores());
+        comic.setColor(comicDTO.getColor());
+        comic.setFechaVenta(comicDTO.getFechaVenta());
+        comic.setEstadoEnum(comicDTO.getEstadoEnum());
+        comic.setCantidad(comicDTO.getCantidad());
+        return comic;
+	}
+	
+	private ComicDTO convertirComicToComicDTO(Comic comic) {
+        ComicDTO comicDTO = new ComicDTO();
+        if(comic.getId()!=null) {
+         comicDTO.setId(comic.getId().toString());
+        }
+        comicDTO.setNombre(comic.getNombre());
+        comicDTO.setEditorial(comic.getEditorial());
+        comicDTO.setTematicaEnum(comic.getTematicaEnum());
+        comicDTO.setColeccion(comic.getColeccion());
+        comicDTO.setNumeroPaginas(comic.getNumeroPaginas());
+        comicDTO.setPrecio(comic.getPrecio());
+        comicDTO.setAutores(comic.getAutores());
+        comicDTO.setColor(comic.getColor());
+        comicDTO.setFechaVenta(comic.getFechaVenta());
+        comicDTO.setEstadoEnum(comic.getEstadoEnum());
+        comicDTO.setCantidad(comic.getCantidad());
+        return comicDTO;
+    }
+	
+	
+}
---
+++ SemilleroHBTSegundaEdicion/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/rest/GestionarComicRest.java
@@ -0,0 +1,43 @@
+/**
+ * GestionarComicRest.java
+ */
+package com.hbt.semillero.rest;
+
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.hbt.semillero.dto.ComicDTO;
+import com.hbt.semillero.ejb.IGestionarComicLocal;
+
+/**
+ * <b>Descripción:<b> Clase que determina
+ * <b>Caso de Uso:<b> 
+ * @author mr-robot
+ * @version 
+ */
+@Path("/GestionarComic")
+public class GestionarComicRest {
+	@EJB
+	private IGestionarComicLocal gestionarComicEJB;
+	
+	@GET
+    @Path("/saludo")
+    @Produces
+	public String primerRest() {
+		return "Prueba inicial servicios rest en el semillero java hbt";
+	}
+	
+	@GET
+    @Path("/consultarComic")
+    @Produces(MediaType.APPLICATION_JSON)
+	public ComicDTO consultarComic(@QueryParam("idComic") Long idComic) {
+		if(idComic!=null) {
+			return gestionarComicEJB.consultarComic(idComic.toString());
+		}
+		return null;
+	}
+}
